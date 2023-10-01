7e7dadac109976308e78fea75a504eafe4101fec
wendybarahona681 <140478500+wendybarahona681@users.noreply.github.com>
2023-10-01 15:03:57 -0600

    cuarta face

--- lib/main.dart
+++ lib/main.dart
@@ -3,32 +3,40 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-   WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp().then((_) {
-  runApp(const MyApp());
-   });
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Crud Firebase',
       theme: ThemeData(
-      
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'CATEGORIAS'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-   final TextEditingController idController = TextEditingController();
+  final String title;
+
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController idController = TextEditingController();
   final ValueNotifier<bool> activoCheckbox = ValueNotifier<bool>(false);
   final ValueNotifier<bool> inactivoCheckbox = ValueNotifier<bool>(false);
   final TextEditingController nombreController = TextEditingController();
@@ -56,101 +64,6 @@ class MyHomePage extends StatefulWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.2),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Icon(Icons.perm_identity),
-                SizedBox(width: 8),
-                Expanded(
-                  child: TextFormField(
-                    controller: idController,
-                    decoration: InputDecoration(labelText: 'ID'),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 16),
-            Row(
-              children: [
-                ValueListenableBuilder<bool>(
-                  valueListenable: activoCheckbox,
-                  builder: (context, value, child) {
-                    return Checkbox(
-                      value: value,
-                      onChanged: (newValue) {
-                        activoCheckbox.value = newValue!;
-                        if (newValue) {
-                          inactivoCheckbox.value = false;
-                        }
-                      },
-                    );
-                  },
-                ),
-                Text('Activo'),
-                ValueListenableBuilder<bool>(
-                  valueListenable: inactivoCheckbox,
-                  builder: (context, value, child) {
-                    return Checkbox(
-                      value: value,
-                      onChanged: (newValue) {
-                        inactivoCheckbox.value = newValue!;
-                        if (newValue) {
-                          activoCheckbox.value = false;
-                        }
-                      },
-                    );
-                  },
-                ),
-                Text('Inactivo'),
-              ],
-            ),
-            SizedBox(height: 16),
-            Row(
-              children: [
-                Icon(Icons.person),
-                SizedBox(width: 8),
-                Expanded(
-                  child: TextFormField(
-                    controller: nombreController,
-                    decoration: InputDecoration(labelText: 'Nombre'),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 16),
-            Container(
-              constraints: BoxConstraints.expand(height: 60),
-              child: ElevatedButton(
-                onPressed: _guardarDatos,
-                child: Text(
-                  'Guardar',
-                  style: TextStyle(fontSize: 24),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  final String title;
-
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -160,7 +73,77 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              MyForm(),
+              Row(
+                children: [
+                  Icon(Icons.perm_identity),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: TextFormField(
+                      controller: idController,
+                      decoration: InputDecoration(labelText: 'ID'),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  ValueListenableBuilder<bool>(
+                    valueListenable: activoCheckbox,
+                    builder: (context, value, child) {
+                      return Checkbox(
+                        value: value,
+                        onChanged: (newValue) {
+                          activoCheckbox.value = newValue!;
+                          if (newValue) {
+                            inactivoCheckbox.value = false;
+                          }
+                        },
+                      );
+                    },
+                  ),
+                  Text('Activo'),
+                  ValueListenableBuilder<bool>(
+                    valueListenable: inactivoCheckbox,
+                    builder: (context, value, child) {
+                      return Checkbox(
+                        value: value,
+                        onChanged: (newValue) {
+                          inactivoCheckbox.value = newValue!;
+                          if (newValue) {
+                            activoCheckbox.value = false;
+                          }
+                        },
+                      );
+                    },
+                  ),
+                  Text('Inactivo'),
+                ],
+              ),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  Icon(Icons.person),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: TextFormField(
+                      controller: nombreController,
+                      decoration: InputDecoration(labelText: 'Nombre'),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              Container(
+                constraints: BoxConstraints.expand(height: 60),
+                child: ElevatedButton(
+                  onPressed: _guardarDatos,
+                  child: Text(
+                    'Guardar',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ),
+              ),
               SizedBox(height: 20),
               ListaRegistros(),
             ],
